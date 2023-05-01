478eef9fabf2456a417b74cc82722dd6c9e17584
Yash Gangwar <gangwaryash06@gmail.com>
2023-05-01 19:12:18 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "AWS-terraform-7558"
+
+    workspaces {
+      name = "ec2-instance-terraform-policy"
+    }
+  }
+}
+ 
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east"
+}
+
+locals {
+  project_name = "terraform"
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-06e46074ae430fba6"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "MyServer"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.my_server.public_ip
+}
